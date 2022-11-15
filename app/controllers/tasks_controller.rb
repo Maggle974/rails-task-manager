c21619280622e9d3497579365e38531a33268101
vincent le callonnec <lecallonnec.v@gmail.com>
2022-11-15 16:41:24 +0100

    exo2 crud

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -14,5 +14,28 @@ class TasksController < ApplicationController
   def create
     @task = Task.new(task_params)
     @task.save
+    redirect_to task_path(@task)
+  end
+
+  def edit
+    @task = Task.find(params[:id]) # avec le before_action
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(task_params)
+    redirect_to task_path(@task)
+  end
+
+  def destroy
+    @task = Task.find(params[:id]) # a commenter avec le before_action
+    @task.destroy
+    redirect_to task_path(@task)
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details)
   end
 end
