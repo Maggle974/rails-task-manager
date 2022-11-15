ea8efe3b845e1d593db6746e5e3b86d640102199
vincent le callonnec <lecallonnec.v@gmail.com>
2022-11-15 15:35:11 +0100

    laundry

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
   # Defines the root path route ("/")
   # root "articles#index"
+  get 'tasks/new', to: 'tasks#new'
+
+  get 'tasks', to: 'tasks#index'
+  get 'tasks/:id', to: 'tasks#show', as: 'task'
+
+  post 'tasks', to: 'tasks#create'
 end
